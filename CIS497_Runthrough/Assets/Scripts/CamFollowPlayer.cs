3d6126aad890643942da4f44e9fddefb056a975e
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-09-02 16:16:46 -0500

    Removed Challenge 1 and Set Up Player

---
+++ CIS497_Runthrough/Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Chris Smith
+* Assignment 2
+* Allows the camera to follow the player.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ CIS497_Runthrough/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Using statement for UI
+using UnityEngine.UI;
+
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    public Text textbox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Finish"))
+        {
+            textbox.text = "You Win!";
+        }
+    }
+}
