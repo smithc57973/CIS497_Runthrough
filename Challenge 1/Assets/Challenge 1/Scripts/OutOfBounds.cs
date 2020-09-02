7b9c5f80ad707d94222cbc9227a5c49bbc3584b7
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-09-02 17:59:57 -0500

    Finished Assignment

---
+++ Challenge 1/Assets/Challenge 1/Scripts/OutOfBounds.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Chris Smith
+* Assignment 2
+* Sets gameOver to false upon falling off the map.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -50 || transform.position.y > 80)
+        {
+            ScoreManager.gameOver = true;
+        }
+    }
+}
